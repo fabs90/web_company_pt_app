cf549dfc923b9a21bcbadd49047407f9fa4dd792
Fabian JC <fabianjuliansyah89@gmail.com>
2025-09-07 08:15:17 +0700

    First commit

---
+++ myScript.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elem = document.querySelector(".masonry-grid");
+//   if (elem) {
+//     new Masonry(elem, {
+//       itemSelector: ".grid-item",
+//       columnWidth: ".grid-sizer",
+//       gutter: 15,
+//       percentPosition: true,
+//     });
+//   }
+// });
