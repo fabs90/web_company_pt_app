d2c322181906dc23f5c34e160214cb224822ad1c
fabian <fabianjuliansyah89@gmail.com>
2025-09-24 21:47:05 +0700

    Add loading animation in download company profile and change all icon to .ico format

--- script-about.js
+++ script-about.js
@@ -8,3 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
   t(), window.addEventListener("scroll", t);
 }),
   AOS.init({ once: !0 });
+
+// Script untuk trigger modal loading
+const downloadLink = document.getElementById("download-link");
+const loadingModal = new bootstrap.Modal(
+  document.getElementById("loadingModal")
+);
+
+downloadLink.addEventListener("click", function () {
+  // Tampilkan loading saat link diklik
+  loadingModal.show();
+
+  // Tutup modal otomatis setelah beberapa detik
+  // (karena browser langsung handle download, kita tidak tahu kapan selesai)
+  setTimeout(() => {
+    loadingModal.hide();
+  }, 3000); // misal 3 detik cukup buat simulasi
+});
